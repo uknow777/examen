47a7bc94fa5e077c96fc6171982c5ca9de97bc7a
Edwin <ftoapanta49@gmail.com>
2025-02-13 10:59:54 -0500

    Añadiendo una nueva funcionalidad

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Página cargada correctamente");
+
+    const form = document.getElementById("form-reserva");
+    const mensaje = document.getElementById("mensaje");
+    const formulario = document.getElementById("reservar");
+    const inputDestino = document.getElementById("destino");
+
+    window.mostrarFormulario = (destino) => {
+        inputDestino.value = destino;
+        formulario.classList.remove("oculto");
+    };
+
+    
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const fecha = document.getElementById("fecha").value;
+        const aerolinea = document.getElementById("aerolinea").value;
+
+        if (fecha) {
+            mensaje.textContent = `¡Tu viaje a ${inputDestino.value} con ${aerolinea} está reservado para el ${fecha}!`;
+        } else {
+            mensaje.textContent = "Por favor, selecciona una fecha válida.";
+        }
+    });
 });
